612804ccdfaa2df916e74a4ab4990f18eb4c8aee
kamal-gaan <kamalpandi.v@gaansoft.com>
2025-01-21 00:49:12 +0530

    inheritance challenges dragon chal

---
+++ ch4_inheri/dragon.py
@@ -0,0 +1,69 @@
+def main():
+    dragons = [
+        Dragon("Green Dragon", 0, 0, 1),
+        Dragon("Red Dragon", 2, 2, 2),
+        Dragon("Blue Dragon", 4, 3, 3),
+        Dragon("Black Dragon", 5, -1, 4),
+    ]
+
+    # don't touch above this line
+
+    for dragon in dragons:
+        describe(dragon)
+        
+    # for dragon in dragons:
+    #     not_fire_breathing_dragons = [d for d in dragons if d != dragon]
+    #     sorted_dragons = sorted(not_fire_breathing_dragons, key=lambda d: dragons.index(d))
+    #     dragon.breathe_fire(3, 3, sorted_dragons)
+    
+    for dragon in dragons:
+        not_fire_breathing_dragons = []
+        for d in dragons:
+            if d != dragon:
+                not_fire_breathing_dragons.append(d)
+        dragon.breathe_fire(3, 3, not_fire_breathing_dragons)
+
+     
+
+# don't touch below this line
+
+
+def describe(dragon):
+    print(f"{dragon.name} is at {dragon.pos_x}/{dragon.pos_y}")
+
+
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        return (
+            self.pos_x >= x_1
+            and self.pos_x <= x_2
+            and self.pos_y >= y_1
+            and self.pos_y <= y_2
+        )
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.__fire_range = fire_range
+
+    def breathe_fire(self, x, y, units):
+        print(f"{self.name} breathes fire at {x}/{y} with range {self.__fire_range}")
+        print("====================================")
+        for unit in units:
+            in_area = unit.in_area(
+                x - self.__fire_range,
+                y - self.__fire_range,
+                x + self.__fire_range,
+                y + self.__fire_range,
+            )
+            if in_area:
+                print(f"{unit.name} is hit by the fire")
+
+
+main()
